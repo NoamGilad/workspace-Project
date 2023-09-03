ca6dd33f855253974247ae11ccfc5420984caa20
NoamGilad <nmgilad@gmail.com>
2023-09-03 22:11:57 +0300

    restructuring

--- workspace/src/pages/SignIn.tsx
+++ workspace/src/pages/SignIn.tsx
@@ -1,31 +1,58 @@
 import { Link, useNavigate, useNavigation } from "react-router-dom";
 import { AuthCtx } from "../contexts/AuthProvider";
 import { useContext, useState } from "react";
-import { AuthProvider } from "../contexts/AuthProvider";
+import { setPersistence, signInWithEmailAndPassword } from "firebase/auth";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 
 const SignInPage = () => {
   const context = useContext(AuthCtx);
-  const navigate = useNavigate();
   const navigation = useNavigation();
+  const storeDataBase = getFirestore();
+  const navigate = useNavigate();
 
   const isSubmitting = navigation.state === "submitting";
-
-  if (!context) {
-    return <div>Loading...</div>;
-  }
-
-  const { auth } = context;
-
-  if (!auth) {
-    return <div>No authentication avilable</div>;
-  }
 
   const onSubmitLoginHandler = (e: React.FormEvent) => {
     e.preventDefault();
 
-    context.onSubmitLoginHandler(e);
+    if (!context) {
+      return <div>Loading...</div>;
+    }
 
-    return navigate("/");
+    if (!context.auth) {
+      return <div>No authentication avilable</div>;
+    }
+
+    // need to set useEffect on firebase's signin function
+    setPersistence(context.auth, { type: "LOCAL" })
+      .then(async () => {
+        try {
+          const userCredential = await signInWithEmailAndPassword(
+            context.auth,
+            context.email,
+            context.password
+          );
+          const user = userCredential.user;
+          console.log(user);
+
+          const userDocRef = doc(storeDataBase, "roles", user.uid);
+          const userDocSnapshot = await getDoc(userDocRef);
+
+          if (userDocSnapshot.exists()) {
+            const userData = userDocSnapshot.data();
+            const currentUserRole = userData.role;
+
+            context.setCurUserRole(currentUserRole);
+          }
+        } catch (error: any) {
+          const errorMessage = error.message;
+          window.alert(errorMessage);
+        }
+        return navigate("/");
+      })
+      .catch((error) => {
+        console.error("Error enabling persistence:", error);
+      });
   };
 
   return (
@@ -36,14 +63,14 @@ const SignInPage = () => {
           <label>Email</label>
           <input
             type="email"
-            onChange={(e) => context.setEmail(e.target.value)}
+            onChange={(e) => context?.setEmail(e.target.value)}
             placeholder="Enter your Email"
             required
           />
           <label>Password</label>
           <input
             type="password"
-            onChange={(e) => context.setPassword(e.target.value)}
+            onChange={(e) => context?.setPassword(e.target.value)}
             placeholder="Enter your password"
             required
           />
