589ba0f1a881dc6b92d6170ccb89ae9b58732a6f
NoamGilad <nmgilad@gmail.com>
2023-09-18 17:18:40 +0300

    new signin & signup

--- workspace/src/pages/SignIn.tsx
+++ workspace/src/pages/SignIn.tsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate, useNavigation } from "react-router-dom";
 import { AuthCtx } from "../contexts/AuthProvider";
-import { useContext, useEffect } from "react";
+import { FormEvent, useContext } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 
@@ -11,49 +11,31 @@ const SignInPage = () => {
 
   const isSubmitting = navigation.state === "submitting";
 
-  useEffect(() => {
-    const onSubmitLoginHandler = async (e: React.FormEvent) => {
-      e.preventDefault();
+  const logInWithEmailAndPassword = async (email: any, password: any) => {
+    try {
+      const userRes = await signInWithEmailAndPassword(
+        context?.auth,
+        email,
+        password
+      );
+      const curUser = userRes.user;
 
-      if (!context) {
-        return <div>Loading...</div>;
-      }
+      console.log(curUser);
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-      if (!context.auth) {
-        return <div>No authentication available</div>;
-      }
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    await logInWithEmailAndPassword(context?.email, context?.password);
+  };
 
-      try {
-        const userCredential = await signInWithEmailAndPassword(
-          context.auth,
-          context.email,
-          context.password
-        );
-        const user = userCredential.user;
-        console.log(user);
-
-        const userDocRef = doc(context.storeDataBase, "roles", user.uid);
-        const userDocSnapshot = await getDoc(userDocRef);
-
-        if (userDocSnapshot.exists()) {
-          const userData = userDocSnapshot.data();
-          const currentUserRole = userData.role;
-
-          context.setCurUserRole(currentUserRole);
-        }
-        navigate("/");
-      } catch (error: any) {
-        const errorMessage = error.message;
-        window.alert(errorMessage);
-      }
-    };
-
-    onSubmitLoginHandler({} as React.FormEvent);
-  }, []);
   return (
     <>
       <h5>Sign in</h5>
-      <form onSubmit={onSubmitLoginHandler}>
+      <form onSubmit={handleSubmit}>
         <main>
           <label>Email</label>
           <input
